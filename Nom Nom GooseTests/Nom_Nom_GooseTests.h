dd635170f4ff413fe4f619821dded68a8aa526c2
iD Student <Student@idtech-id02.princeton.edu>
2012-07-02 11:03:06 -0400

    Initial Commit

---
+++ Nom Nom GooseTests/Nom_Nom_GooseTests.h
@@ -0,0 +1,13 @@
+//
+//  Nom_Nom_GooseTests.h
+//  Nom Nom GooseTests
+//
+//  Created by iD Student on 7/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Nom_Nom_GooseTests : SenTestCase
+
+@end
---
+++ Nom Nom GooseTests/Nom_Nom_GooseTests.m
@@ -0,0 +1,32 @@
+//
+//  Nom_Nom_GooseTests.m
+//  Nom Nom GooseTests
+//
+//  Created by iD Student on 7/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Nom_Nom_GooseTests.h"
+
+@implementation Nom_Nom_GooseTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Nom Nom GooseTests");
+}
+
+@end
